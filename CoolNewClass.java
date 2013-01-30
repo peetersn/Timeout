fec595f11707c5e60d1906cc8ded2e0a6fd805e4
Nicolas Peeters <peetersn@gmail.com>
2013-01-30 15:35:15 +0100

    added a new class.

---
+++ CoolNewClass.java
@@ -0,0 +1,11 @@
+package com.myapp.service.aop;
+
+/**
+ * //TODO please create a concise class description
+ *
+ * @author npeeters
+ */
+public class CoolNewClass {
+
+    private int numberOfProblems = 99;
+}
